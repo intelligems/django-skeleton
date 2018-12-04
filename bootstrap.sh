31ab4ef89d0e15cbbbfd4fd6588938e1357de8be
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-12-04 15:43:50 +0200

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -U jinja2, pyyaml
+pip install -U jinja2 pyyaml
 # Needed to install jinja2 and pyyaml requirements to run the templating script.
 
 python -m config_templates.chartmap
